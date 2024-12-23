0c0878b4c8b8fcc16dd2f6cb95665d961c4d2a13
blackmamba2004 <vasilsemen04@gmail.com>
2024-12-23 20:56:21 +0300

    Модели
    1.Определен класс BaseModel, хранящий метаданные. 2.Определена модель User.

---
+++ backend/models/user.py
@@ -0,0 +1,17 @@
+from sqlalchemy import text
+from sqlalchemy.orm import Mapped, mapped_column
+
+from backend.enums.user import Gender
+from backend.models.base import BaseModel
+
+
+class User(BaseModel):
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    first_name: Mapped[str] = mapped_column(index=True, nullable=True)
+    last_name: Mapped[str] = mapped_column(index=True, nullable=True)
+    patronymic: Mapped[str] = mapped_column(index=True, nullable=True)
+    email: Mapped[str] = mapped_column(unique=True, index=True, nullable=False)
+    hashed_password: Mapped[str] = mapped_column(nullable=False)
+    gender: Mapped[Gender | None] = mapped_column(default=Gender.MALE, server_default=text("'MALE'"))
+    is_active: Mapped[bool] = mapped_column(default=True)
+    is_superuser: Mapped[bool] = mapped_column(default=False)
